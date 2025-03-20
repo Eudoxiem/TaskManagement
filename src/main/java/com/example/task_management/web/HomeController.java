521a03dc3bf0b6e6e2cf2a96591269b64eb6579d
Eudoxie <mwinkeudoxie@yahoo.fr>
2025-03-20 18:13:00 +0100

    index.html  css bootstrap+ajout dépendance

--- src/main/java/com/example/task_management/web/HomeController.java
+++ src/main/java/com/example/task_management/web/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.task_management.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
